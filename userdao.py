84d39882b0949ce13e112fbc10343c5af2bb6502
Aidan Killeen <aidan.killeen@gmail.com>
2026-03-04 10:16:50 +0000

    contining with userdao.py

--- userdao.py
+++ userdao.py
@@ -9,6 +9,9 @@ class UserDao():
     def __init__(self, filename="C:\\work\\training\\databases\\userdb.db"):
         self.conn = sqlite3.connect(filename)
 
+    def close(self):
+        self.conn.close()
+
     def get_all(self):
         # return all users from the db
         users = []
@@ -28,16 +31,42 @@ class UserDao():
         pass
     def delete(self, id):
         # delete user id from the db
-        pass
+        sql = f"DELETE FROM users WHERE id={id}"
+        self.conn.execute(sql)
+        self.conn.commit()
+        
     def update(self, user):
         # update this user in the db
-        pass
+        sql = f"""UPDATE users
+                    SET name = ?, 
+                        email= ?, 
+                        active= ?
+                    WHERE id = ?"""
+        self.conn.execute(sql, 
+            (user.name, user.email, 1 if user.active else 0, user.id))
+        self.conn.commit()
+
     
 print ("UserDao tests")    
 dao = UserDao()
+
+dao.delete(15)
+
+users = dao.get_all()
+
+print (users[-1])
+
+user_to_update = users[-1]
+user_to_update.name = "changed"
+user_to_update.email = "changed@gmail.com"
+user_to_update.active = not user_to_update.active
+
+dao.update(user_to_update)
+
 users = dao.get_all()
 
 for user in users:
     print (user)
 
 
+dao.close()
