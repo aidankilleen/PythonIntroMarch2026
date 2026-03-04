a7b05003ba2d4fd3ef9e430bb5a17f547c8175a6
Aidan Killeen <aidan.killeen@gmail.com>
2026-03-04 09:57:11 +0000

    added get_all to userdao.py

--- userdao.py
+++ userdao.py
@@ -1,26 +1,43 @@
 # userdao.py
 
+import sqlite3
+
+from user import User
+
 class UserDao():
+
+    def __init__(self, filename="C:\\work\\training\\databases\\userdb.db"):
+        self.conn = sqlite3.connect(filename)
+
     def get_all(self):
         # return all users from the db
-        pass
+        users = []
+        sql = "SELECT * FROM users"
+
+        cur = self.conn.execute(sql)
+
+        for rec in cur:
+            id, name, email, active = rec
+            user = User(id, name, email, active)
+            users.append(user)
+
+        return users
 
     def add(self, user):
         # add this user to the db
         pass
-
     def delete(self, id):
         # delete user id from the db
         pass
-
     def update(self, user):
         # update this user in the db
         pass
     
+print ("UserDao tests")    
 dao = UserDao()
 users = dao.get_all()
 
-#for user in users:
-#    print (user.name)
+for user in users:
+    print (user)
 
 
