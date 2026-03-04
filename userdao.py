5a78be03207dd2892c714ceb9b9417e2c69e4274
Aidan Killeen <aidan.killeen@gmail.com>
2026-03-04 09:47:26 +0000

    started userdao.py

--- userdao.py
+++ userdao.py
@@ -1,5 +1,26 @@
 # userdao.py
 
 class UserDao():
-    pass
+    def get_all(self):
+        # return all users from the db
+        pass
+
+    def add(self, user):
+        # add this user to the db
+        pass
+
+    def delete(self, id):
+        # delete user id from the db
+        pass
+
+    def update(self, user):
+        # update this user in the db
+        pass
+    
+dao = UserDao()
+users = dao.get_all()
+
+#for user in users:
+#    print (user.name)
+
 
